c013c8a2f110fe4c1145543932ec49fd0dbe1b40
Alistair McKinlay <github@10people.co.uk>
2014-12-01 21:43:08 +0000

    I'm like, totally a mod now, no need to pretend. Fix #43

--- api/policies/isMod.js
+++ api/policies/isMod.js
@@ -7,7 +7,7 @@ module.exports = function(req, res, next){
   http.get("http://www.reddit.com/r/pokemontrades/about/moderators.json?user=" + req.user.name, function (res) {
     res.setEncoding("utf8");
     res.on("data", function (body) {
-      if (JSON.parse(body).data.children.length === 1 || req.user.name === "YaManicKill") {
+      if (JSON.parse(body).data.children.length === 1) {
         req.user.isMod = true;
       }
       return next();

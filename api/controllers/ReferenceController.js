50bc62453dae8662a8b96e814f4846ca0950b2ff
not-an-aardvark <notaardvark@gmail.com>
2015-10-19 17:38:15 -0400

    The Approve All button and deletion both work now

--- api/controllers/ReferenceController.js
+++ api/controllers/ReferenceController.js
@@ -146,6 +146,30 @@ module.exports = {
         return res.serverError(err);
       }
       if (ref.user === req.user.id || req.user.isMod) {
+        if (ref.verified) {
+          User.findOne({name: ref.user2.substring(3)}, function (err, otherUser) {
+            if (err) {
+              console.log("Warning: The trade being deleted is marked 'verified', but does not appear to have a complement.");
+              return;
+            }
+            User.findOne({id: ref.user}, function (err2, thisUser) {
+              if (err2 || !thisUser) {
+                return res.status(404).json("User not found");
+              }
+              var query = {
+                user: otherUser.id,
+                url: new RegExp(ref.url.substring(ref.url.indexOf("/r/"))),
+                user2: '/u/' + thisUser.name
+              };
+              //If a verified reference is deleted, its compelmentary reference is un-verified.
+              Reference.update(query, {verified: false}, function (err, otherRef) {
+                if (err) {
+                  console.log("Error while updating complementary trade.");
+                }
+              });
+            });
+          });
+        }
         Reference.destroy({id: id})
           .exec(function (err, refs) {
             if (err) {
@@ -155,7 +179,7 @@ module.exports = {
             }
           });
       } else {
-        return res.forbidden("unauthorised");
+        return res.forbidden();
       }
     });
 
@@ -200,45 +224,51 @@ module.exports = {
   },
 
   approve: function (req, res) {
-    console.log("test");
     if (!req.user.isMod) {
       return res.json("Not a mod", 403);
     }
 
-    var refUserId = req.allParams().userid,
-      id = req.allParams().id,
+    var id = req.allParams().id,
       approve = req.allParams().approve;
 
-    User.findOne({id: refUserId}, function (err, refUser) {
+    User.findOne({id: req.allParams().userid}, function (err, refUser) {
       if (!refUser) {
         return res.notFound("User not found");
       }
       Reference.findOne(id, function (err, ref) {
-        References.approve(ref, res).then(function (result) {
+        References.approve(ref, refUser.name, approve).then(function (result) {
           return res.status(200).json(ref);
         }, function (error) {
-          return res.status(error).end();
+          return res.json(error);
         });
       });
     });
   },
 
   approveAll: function (req, res) {
-    var refUserId = req.allParams().userid,
-      type = req.allParams().type;
+    if (!req.user.isMod) {
+      return res.json("Not a mod", 403);
+    }
 
-    User.findOne({id: refUserId}, function (err, refUser) {
+    User.findOne({id: req.allParams().userid}, function (err, refUser) {
       if (!refUser) {
         return res.notFound("User not found");
       }
       var promises = [];
-      Reference.find(req.id).forEach(function (ref) {
-        promises.push(References.approve(ref));
-      });
-      Promise.all(promises).then(function (result) {
-        return res.status(200).json(result);
-      }, function (error) {
-        return res.status(error).end();
+      Reference.find({user: refUser.id, type: req.allParams().type}, function (err, refs) {
+        if (err) {
+          return res.serverError(err);
+        }
+        console.log(refs);
+        for (var i = 0; i < refs.length; i++) {
+          promises.push(References.approve(refs[i], refUser.name, true));
+        }
+        Promise.all(promises).then(function (err, result) {
+          return res.status(200).json(result);
+        }, function (error) {
+          console.log(error);
+          return res.status(error.statusCode).json(error);
+        });
       });
     });
   },

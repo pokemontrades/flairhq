1f05fcadeb595c3e7736880c343fc05b5ef284b6
not-an-aardvark <notaardvark@gmail.com>
2015-10-23 21:40:09 -0400

    Flair app list now displays requirements on mouseover

--- assets/js/adminCtrl.js
+++ assets/js/adminCtrl.js
@@ -83,8 +83,54 @@ define([
             });
         };
 
+        $scope.getFlairs = function () {
+          var url = "/flair/all";
+          io.socket.get(url, function (data, res) {
+            if (res.statusCode === 200) {
+              $scope.flairs = data;
+              if (data.length === 0) {
+                $scope.flairs[0] = {
+                  name: "",
+                  trades: "",
+                  shinyevents: "",
+                  eggs: "",
+                  sub: ""
+                };
+              }
+            }
+          });
+        };
+
+        $scope.formattedRequirements = function (flair) {
+            var reqs;
+            for (var i = 0; i < $scope.flairs.length; i++) {
+                if ($scope.flairs[i].name === flair) {
+                    reqs = $scope.flairs[i];
+                }
+            }
+            if (!reqs) {
+                return 'Unknown requirements';
+            }
+            var formatted = '';
+            if (reqs.trades) {
+                formatted += reqs.trades + (reqs.trades > 1 ? ' trades, ' : ' trade, ');
+            }
+            if (reqs.involvement) {
+                formatted += reqs.involvement + (reqs.involvement > 1 ? ' free tradebacks/redemptions, ' : 'free tradeback/redemption, ');
+            }
+            if (reqs.giveaways) {
+                formatted += reqs.giveaways + (reqs.giveaways > 1 ? ' giveaways, ' : 'giveaway, ');
+            }
+            if (reqs.eggs) {
+                formatted += reqs.eggs + (reqs.eggs > 1 ? ' hatches, ' : ' hatch, ');
+            }
+            formatted = formatted.slice(0,-2);
+            return formatted;
+        };
+
         $scope.getBannedUsers();
         $scope.getFlairApps();
+        $scope.getFlairs();
     };
 
     return adminCtrl;

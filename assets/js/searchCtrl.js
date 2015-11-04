a16ae1af6d8934bbc7f1d280e5054c4379a93cd9
not-an-aardvark <notaardvark@gmail.com>
2015-11-03 20:56:46 -0500

    Added timestamp to logs

--- assets/js/searchCtrl.js
+++ assets/js/searchCtrl.js
@@ -93,11 +93,14 @@ module.exports = function ($scope, $timeout, UserFactory) {
     $scope.searchedFor = url;
     io.socket.get(url, function (data, res) {
       if (res.statusCode === 200 && $scope.searchedFor === url) {
-        console.log($scope.searchResults);
         if (skip) {
           $scope.searchResults = $scope.searchResults.concat(data);
         } else {
           $scope.searchResults = data;
+        }
+        for (var i = 0; i < $scope.searchResults.length; i++) {
+          //Otherwise the date ends up in a messy format like 2015-01-01T00:00:00.000Z
+          $scope.searchResults[i].createdAt = $scope.searchResults[i].createdAt.replace("T", " ").replace(/\.\d*Z/, " UTC");
         }
         $scope.searching = false;
         $scope.$apply();

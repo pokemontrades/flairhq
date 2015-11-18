cdc8047664d075d4f973e02b6e488352582c81f0
not-an-aardvark <notaardvark@gmail.com>
2015-11-18 02:43:06 -0500

    Fix #410

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -42,7 +42,7 @@ module.exports = {
                 }
               });
               if (deleteGame) {
-                promises.push(Game.destroy(game.id));
+                promises.push(Game.destroy(game.id).exec(function () {}));
               }
             });
           });

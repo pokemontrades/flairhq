b51cad9ebedbd21dfd00946d572dd0bfc83fe959
Alistair McKinlay <alistair@10people.co.uk>
2015-11-22 19:46:01 +0000

    Move tooltip stuff to feature folder, condense to 1 module.

--- assets/tooltip/tooltip.module.js
+++ assets/tooltip/tooltip.module.js
@@ -1,14 +1,21 @@
 var ng = require("angular");
 var $ = require('jquery');
 
-ng.module("genericTooltipModule", []).directive("ngGenericTooltip", function () {
+ng.module("tooltipModule", []).directive("ngTooltip", function () {
   return {
     restrict: 'E',
     replace: true,
     scope: {
-      title: '@title'
+      title: '@title',
+      label: '@label'
     },
-    templateUrl: '/common/genericTooltipView.html',
+    templateUrl: function (tElement, tAttrs) {
+      if (tAttrs.unlabeled) {
+        return '/tooltip/tooltip.view.html';
+      } else {
+        return '/tooltip/label.view.html';
+      }
+    },
     transclude: true,
     link: function (scope, element) {
       var thisElement = $(element[0]).find('[data-toggle=tooltip]');

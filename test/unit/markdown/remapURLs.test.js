a8854ebd0c5bc625f38a0a321814aeb4a3698942
Alistair McKinlay <github@10people.co.uk>
2015-11-23 12:16:57 +0000

    Add flairhq link next to user links

--- test/unit/markdown/remapURLs.test.js
+++ test/unit/markdown/remapURLs.test.js
@@ -3,14 +3,14 @@ var remapURLs = require("../../../assets/markdown/remapURLs");
 
 describe("replaceUserURLS", function () {
   it("Replaces /u/test with http://www.reddit.com/u/test", function () {
-    var test = remapURLs('<a href="/u/test"></a>');
-    assert.equal(test, '<a href="https://www.reddit.com/u/test"></a>', "Not mapping user urls correctly.")
+    var test = remapURLs('<a href="/u/test">/u/test</a>');
+    assert.equal(test, '<a href="https://www.reddit.com/u/test">/u/test</a> (<a href="/u/test">FlairHQ</a>)', "Not mapping user urls correctly.")
   });
 });
 
 describe("replaceSubURLS", function () {
   it("Replaces /u/test with http://www.reddit.com/u/test", function () {
-    var test = remapURLs('<a href="/r/test"></a>');
-    assert.equal(test, '<a href="https://www.reddit.com/r/test"></a>', "Not mapping sub urls correctly.")
+    var test = remapURLs('<a href="/r/test">/r/test</a>');
+    assert.equal(test, '<a href="https://www.reddit.com/r/test">/r/test</a>', "Not mapping sub urls correctly.")
   });
 });

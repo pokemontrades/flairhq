3b05facbe3d3b4e2492e86cbd3639bc0b5a50aac
Alistair McKinlay <github@10people.co.uk>
2015-11-23 12:16:57 +0000

    Add flairhq link next to user links

--- assets/markdown/remapURLs.js
+++ assets/markdown/remapURLs.js
@@ -1,4 +1,6 @@
 module.exports = function (value) {
-  var regex = /(href=")(\/[ur]\/[a-zA-Z_\/-]+)(")/g;
-  return value.replace(regex, "$1https://www.reddit.com$2$3");
+  var userRegex = /(<a href=")(\/[u]\/[a-zA-Z_\/-]+)(">)(.*)(<\/a>)/g;
+  var usersReplaced = value.replace(userRegex, "$1https://www.reddit.com$2$3$4$5 ($1$2$3FlairHQ$5)");
+  var subRegex = /(<a href=")(\/[r]\/[a-zA-Z_\/-]+)(">)(.*)(<\/a>)/g;
+  return usersReplaced.replace(subRegex, "$1https://www.reddit.com$2$3$4$5");
 };

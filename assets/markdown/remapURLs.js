5668e82643ffa05b080574e8b1db9abacae39f93
Alistair McKinlay <alistair@10people.co.uk>
2015-11-23 20:23:09 +0000

    Adding the tasks outside the watch doesn't make it quicker.

--- assets/markdown/remapURLs.js
+++ assets/markdown/remapURLs.js
@@ -1,6 +1,6 @@
 module.exports = function (value) {
-  var userRegex = /(<a href=")(\/[u]\/[a-zA-Z_\/-]+)(">)\/?\2(.*?)(<\/a>)/g;
-  var userReplaced = value.replace(userRegex, "$1https://www.reddit.com$2$3$2$5 ($1$2$3FlairHQ$5)");
-  var subRegex = /(<a href=")(\/[r]\/[a-zA-Z_\/-]+)(">)\/?\2(.*?)(<\/a>)/g;
-  return userReplaced.replace(subRegex, "$1https://www.reddit.com$2$3$2$5");
+  var userRegex = /(<a href=")\/(u\/[a-zA-Z0-9_\/-]+)(">)\/?\2(.*?)(<\/a>)/g;
+  var userReplaced = value.replace(userRegex, "$1https://www.reddit.com/$2$3$2$5 ($1$2$3FlairHQ$5)");
+  var subRegex = /(<a href=")\/(r\/[a-zA-Z0-9_\/-]+)(">)\/?\2(.*?)(<\/a>)/g;
+  return userReplaced.replace(subRegex, "$1https://www.reddit.com/$2$3$2$5");
 };

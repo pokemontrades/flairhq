9493ae99d59f439f8ed8ccf069c7a86523ec597e
Teddy Katz <teddy.katz@gmail.com>
2016-11-28 03:50:58 -0500

    Minify files when running on production (#563)

--- assets/search/search.module.js
+++ assets/search/search.module.js
@@ -6,6 +6,6 @@ var angular = require("angular");
 var controller = require("./search.controller.js");
 
 var searchModule = angular.module("fapp.search", []);
-searchModule.controller("SearchController", controller);
+searchModule.controller("SearchController", ['$scope', '$timeout', controller]);
 
 module.exports = searchModule;
--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
   grunt.registerTask('prod', [
     'compileProd',
     'concat',
+    'uglify',
     'cssmin',
     'sails-linker:prodJs',
     'sails-linker:prodStyles'

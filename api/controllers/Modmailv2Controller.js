7f695a56fe33212d3eedeb6e9ba3296fc3544e4b
robdy <robdyjas@gmail.com>
2018-08-31 23:59:33 +0200

    fixed conversation adding

--- api/controllers/Modmailv2Controller.js
+++ api/controllers/Modmailv2Controller.js
@@ -13,10 +13,9 @@ module.exports = {
     
     let resultsAll = (await Reddit.getNewModmail(refreshToken, ['pokemontrades','svexchange'], '' , 'all')); // '347q1'
     let resultsMod = (await Reddit.getNewModmail(refreshToken, ['pokemontrades','svexchange'], '' , 'mod'));
-    let resultsNot = (await Reddit.getNewModmail(refreshToken, ['pokemontrades','svexchange'], '' , 'notifications'));
     let resultsArc = (await Reddit.getNewModmail(refreshToken, ['pokemontrades','svexchange'], '' , 'archived'));
-    let allResults = [Array.prototype.concat(resultsAll[0], resultsMod[0], resultsNot[0], resultsArc[0])];
-    // inprogress, new, highlighted are not necessary
+    let allResults = [Array.prototype.concat(resultsAll[0], resultsMod[0], resultsArc[0])];
+    // inprogress, new, notifications, highlighted are not necessary
     
     let allConversations = Modmailv2.formatConversations(allResults);
     let allMessages      = Modmailv2.formatMessages(allResults);
@@ -33,16 +32,27 @@ module.exports = {
         .catch( e => {console.log(e)});
     }
     if (allConversations.length > 0) {
-      let convAdd   = await ModmailConv
-      .findOrCreate(allConversations) /* NEED TO FINISH UPDATING PART
+      let convAdd   = await allConversations.map(conversation => {
+        ModmailConv.findOrCreate({ id: conversation.id }, conversation)
+        .exec(function(err, newOrExistingRecord, wasCreated) {
+          if (err) { sails.log.error(err); }
+          //console.log (newOrExistingRecord);
+          //console.log (wasCreated);
+          //console.log (conversation.id);
+          return newOrExistingRecord;
+        })
+      })
+    };
+
+      /*.findOrCreate(allConversations) /* NEED TO FINISH UPDATING PART
       .exec(function(err, newOrExistingRecord, wasCreated) {
         if (!(wasCreated) && newOrExistingRecord.lastUpdated < allConversations.lastUpdated ) {
 
         }
-      })*/
-      .catch( e => {console.log(e)});
-    }
+      })
+      .catch( e => {console.log(e)});*/
 
+/*
     // Create associations (have to add actions)
     let promises = [];
     allConversations.forEach(element => {
@@ -52,7 +62,7 @@ module.exports = {
       return res.ok(results);
     }, function (error) {
       return res.serverError(error);
-});
+});*/
     //res.ok(allConversations);
   },
 

e31f1e7d435dbed0ffd30c5ea7ed8e71b1d234d8
TSAR <TheSonAlsoRises@users.noreply.github.com>
2021-11-18 22:32:10 +0100

    Update userCtrl.js

--- assets/userCtrl.js
+++ assets/userCtrl.js
@@ -291,7 +291,7 @@ module.exports = function ($scope, $location, io) {
   };
 
   $scope.possibleConsoles = ["Switch", "3DS"];
-  $scope.possibleGames = ["X", "Y", "ΩR", "αS", "S", "M", "US", "UM", "LGP", "LGE", "SW", "SH"];
+  $scope.possibleGames = ["X", "Y", "ΩR", "αS", "S", "M", "US", "UM", "LGP", "LGE", "SW", "SH", "BD", "SP"];
 
   $scope.setFlairText = function () {
     $("#setTextError").html("").hide();
